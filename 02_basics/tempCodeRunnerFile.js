9e6564f74304e9b0d89c184ad0eacfcc48c7213a
Arsalan Siddiqui <siddiquiarsalan22as@gmail.com>
2025-09-18 13:06:54 +0000

    objects part 1

--- 02_basics/tempCodeRunnerFile.js
+++ 02_basics/tempCodeRunnerFile.js
@@ -1,6 +1,4 @@
-
-// let score1 = 100
-// let score2 = 200
-// let score3 = 300
-
-// console.log(Array.of(score1, score2, score3));
+console.log(JsUser.email)
+console.log(JsUser["email"])
+console.log(JsUser["full name"])
+console.log(JsUser[mySym])
