58b8faa96bf461b1bd415ea941d4215ed80a4495
Arsalan Siddiqui <siddiquiarsalan22as@gmail.com>
2025-10-08 23:42:26 +0000

    getter setter

--- 10_classes_and_oop/tempCodeRunnerFile.js
+++ 10_classes_and_oop/tempCodeRunnerFile.js
@@ -1,2 +1,22 @@
-console.log(userOne);
-// console.log(userTwo);
+// ES6
+
+// class User {
+//     constructor(username, email, password){
+//         this.username = username;
+//         this.email = email;
+//         this.password = password
+//     }
+
+//     encryptPassword(){
+//         return `${this.password}abc`
+//     }
+//     changeUsername(){
+//         return `${this.username.toUpperCase()}`
+//     }
+
+// }
+
+// const chai = new User("chai", "chai@gmail.com", "123")
+
+// console.log(chai.encryptPassword());
+// console.log(chai.changeUsername());
